607d24cd0f5680ca1262105e054000e3b8f3d86e
Queen Swain <queenswain0@gmail.com>
2019-02-09 15:22:03 +0100

    Product Management(Store,Edit,Delete)  Pages built and tested

--- MyShop.DataAccess.InMemory/ProductRepository.cs
+++ MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -8,7 +8,7 @@ using MyShop.Core.Models;
 
 namespace MyShop.DataAccess.InMemory
 {
-    class ProductRepository
+   public class ProductRepository
     {
         ObjectCache cache = MemoryCache.Default;
         List<Product> products;
@@ -18,7 +18,7 @@ namespace MyShop.DataAccess.InMemory
         public ProductRepository()
         {
             products = cache["products"] as List<Product>;
-            if(products!=null)
+            if(products==null)
             {
                 products = new List<Product>();
             }
