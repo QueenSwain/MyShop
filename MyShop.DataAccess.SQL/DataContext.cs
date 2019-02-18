342819b491276b5f4f572a8845007b888845e057
Queen Swain <queenswain0@gmail.com>
2019-02-18 22:01:39 +0100

    hange in DataContext

--- MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop.DataAccess.SQL/DataContext.cs
@@ -12,8 +12,6 @@ namespace MyShop.DataAccess.SQL
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
-
-
         
     }
   
